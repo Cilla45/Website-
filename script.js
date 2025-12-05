64d340de0b5b40bfc7b873afc65fcb096832465f
Cilla45 <arhin1928@gmail.com>
2025-12-05 10:14:51 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(e.target.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+});
